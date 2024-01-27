614631b94ee5055f068320f5b12e50c09e14cc6a
Asad Saeed <asadsaeed12225@gmail.com>
2024-01-27 11:35:21 +0500

    feat:Basic Routes

---
+++ begineer/routes.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const members = require("./data");
+
+const port = 5000;
+// app modules get,post,put,patch,delete,all,use,listen
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Home Page</h1>");
+});
+app.get("/about", (req, res) => {
+  res.status(200).send("<h1>About Page</h1>");
+});
+app.get("/table", (req, res) => {
+  // res.status(200).sendFile(path.join(__dirname,"public","index1.html"));
+  res.status(200).sendFile(path.resolve(__dirname, "public", "index2.html"));
+});
+// // Rest Api
+
+app.get("/api/members", (req, res) => {
+  res.status(200).json(members);
+});
+// // Dont use above method t show file use express use to set static folder
+// Set Static folder
+// app.use(express.static(path.join(__dirname,"public")))
+// make this at last of every request
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+app.listen(port, () => {
+  console.log(`Example app is running on port ${port}`);
+});
