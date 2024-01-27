2d6502f29522a87752a5ec1bf166811fc8878bd0
Asad Saeed <asadsaeed12225@gmail.com>
2024-01-27 16:08:19 +0500

    feat:Create a member

--- begineer/index.js
+++ begineer/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const logger = require("./middleware/logger");
 const port = 5000;
+// Init middleware {middleware always use before route}
+// Every time we make a request we see hello print
+// app.use(logger);
+// Boday Parser middleware for POST
+app.use(express.json()); //handle raw json
+app.use(express.urlencoded({extended:false})); //handle form submittion for url encoded data
 // Members api routes
 app.use("/api/members", require("./routes/api/members"));
-// Init middleware
-// Every time we make a request we see hello print
-app.use(logger);
+
 
 app.listen(port, () => {
   console.log(`Example app is running on port ${port}`);
--- begineer/routes/api/members.js
+++ begineer/routes/api/members.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const members = require("../../data");
+const uuid = require("uuid");
 
 // Get All member
 router.get("/", (req, res) => {
@@ -19,4 +20,20 @@ router.get("/:id", (req, res) => {
   }
 });
 
+// Create a Member
+router.post("/", (req, res) => {
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: req.body.status,
+  };
+  if (!newMember.name && !newMember.email) {
+    return res
+      .status(400)
+      .json({ message: "Name and Email are required fields" });
+  }
+  members.push(newMember);
+  res.status(201).json(members);
+});
 module.exports = router;
